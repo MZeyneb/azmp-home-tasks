908bc5928e6c8ac670851d32cbf52c7cdc781b19
mzeyneb <zeynebmustafa05@gmail.com>
2024-10-31 19:27:07 +0400

    31.10 half of homework

---
+++ 31.10js/script.js
@@ -0,0 +1,47 @@
+let array = [12, 23, 43, 54, 65, 67, 89, 90, 100, 12];
+// let eded = +prompt("eded daxil edin: ")
+// for (let i = 0; i < array.length; i++) {
+//     if(array[i] == eded){
+//         alert(`${array[i]} tekrar olunub.`);
+//     }
+//     else{
+//         alert("tekrar yoxdur.")
+//         break;
+//     }
+    
+    
+// }
+
+
+// 15
+
+const students = [
+    { name: "Ali", scores: [90, 85, 92] },
+    { name: "Mammal", scores: [75, 80, 85] },
+    { name: "Camil", scores: [90, 95, 85] },
+    { name: "Davud", scores: [100, 100, 100] }
+  ];
+
+
+
+
+for (let i = 0; i < students.length; i++) {
+    let cem = 0;
+    let ortalama = 0;
+    let skor = [];
+    for (let j = 0; j < (students[i].scores).length; j++) {
+        cem += students[i].scores[j];
+        
+        
+    }
+
+    ortalama = cem / (students[i].scores).length;
+    
+    
+    
+    console.log(`${students[i].name} ortalama: ${ortalama}`);
+    if(ortalama > 90 ){
+        console.log(`${students[i].name} ortalamasi 90-dan boyukdur.`);
+
+    }
+}
