4154f2981cb2a367ec76e33a0202272a2fbd1f6a
mzeyneb <zeynebmustafa05@gmail.com>
2024-11-03 22:56:43 -0800

    task 3 done

--- 02.JSfunctions (02.11.24)/script.js
+++ 02.JSfunctions (02.11.24)/script.js
@@ -22,24 +22,22 @@
 // moving([3,6,2,8,5,7], 0, 4)
 
 // 3
-// -------------
+
 // function repeated(array){
 //     let result = [];
-//     let flag = true;
 //     for (let i = 0; i < array.length; i++) {
-//         for (let j = i+1; j < array.length; j++) {
-//             if( array[i] == array[j] && flag){
-//                 result.push(array[i])
-//                 flag = false;
+//         let flag = false;
+//         for (let j = 0; j < result.length; j++) {
+//             if( array[i] == array[j]){
+//                 flag = true;
 //                 break;
                 
 //             }
-//             else if(array[i] != array[j]){
-                
-//                 flag = true
-               
-//             }
+            
 
+//         }
+//         if (!flag){
+//             result.push(array[i])
 //         }
        
         
