f557f788a3e58e7166b02cf69e41dbda82c330f4
mzeyneb <zeynebmustafa05@gmail.com>
2024-11-19 00:39:09 +0400

    todo task done

--- 17.11js(todo)/script.js
+++ 17.11js(todo)/script.js
@@ -1,6 +1,6 @@
-const todos = [];
+const todos = JSON.parse(localStorage.getItem("todos")) || [];
 const added = document.querySelector(".added");
-const add = document.querySelector(".add")
+const add = document.querySelector(".add");
 const inputodo = document.querySelector(".todo");
 const all = document.querySelector(".all");
 const completed = document.querySelector(".completed");
@@ -13,153 +13,133 @@ const edit = document.querySelector(".edit-bg");
 const cedit = document.querySelector(".cancel-edit");
 const save = document.querySelector(".save");
 
-
 let done = false;
 let current = null;
 
-function addingg(){
-    const todoText = inputodo.value.trim()
-    const todorow = document.createElement("div");
-    todorow.classList.add("block");
-    todorow.innerHTML= `
-    <input type="checkbox" class="scales" />
-    <h2>${todoText}</h2>
-
-    <div class="icons">
-    <i class="fa-solid fa-trash delete"></i>
-    <i class="fa-regular fa-pen-to-square edit"></i>
-    </div>
-     
-    `
-    added.appendChild(todorow)
-
-    inputodo.value = "";
-    todos.push(todoText)
-
-
-
-    const checkbox = todorow.querySelector(".scales")
-    const checktext = todorow.querySelector(".block h2")
-    const block = todorow.querySelector(".block")
-
-    
-    checkbox.addEventListener("click", function(){
-        if(checkbox.checked){
-            checktext.style.textDecoration = "line-through";
-            done = true;
-            
-        }
-        else{
-            checktext.style.textDecoration = "none";
-            done = false
-            
-        }
-    })
-
-    
-    const deletee = todorow.querySelector(".delete");
-    const editt = todorow.querySelector(".edit")
-    
-    deletee.addEventListener("click", function(){
-        todorow.remove()
-
-        const index = todos.indexOf(todoText);
-        if(index!== -1){
-            todos.splice(index, 1);
-        }
-    })
-
-    editt.addEventListener("click", function(){
-        edit.style.display = "flex"
-        const editing = document.querySelector(".editing");
-        editing.value = todoText;
-        current = todorow;
-  
-
-    })
-    
-    
+function updateLocalStorage() {
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-save.addEventListener("click", function(){
+function loadTodos() {
+  added.innerHTML = "";
+  todos.forEach((todo) => {
+    createTodoElement(todo.text, todo.done);
+  });
+}
+
+function createTodoElement(todoText, isDone = false) {
+  const todorow = document.createElement("div");
+  todorow.classList.add("block");
+  todorow.innerHTML = `
+        <input type="checkbox" class="scales" ${isDone ? "checked" : ""} />
+        <h2 style="text-decoration: ${
+          isDone ? "line-through" : "none"
+        };">${todoText}</h2>
+        <div class="icons">
+            <i class="fa-solid fa-trash delete"></i>
+            <i class="fa-regular fa-pen-to-square edit"></i>
+        </div>
+    `;
+  added.appendChild(todorow);
+
+  const checkbox = todorow.querySelector(".scales");
+  const checktext = todorow.querySelector(".block h2");
+
+  checkbox.addEventListener("click", function () {
+    const index = todos.findIndex((todo) => todo.text === todoText);
+    if (index !== -1) {
+      todos[index].done = checkbox.checked;
+      updateLocalStorage();
+    }
+    checktext.style.textDecoration = checkbox.checked ? "line-through" : "none";
+  });
+
+  const deletee = todorow.querySelector(".delete");
+  deletee.addEventListener("click", function () {
+    todorow.remove();
+    const index = todos.findIndex((todo) => todo.text === todoText);
+    if (index !== -1) {
+      todos.splice(index, 1);
+      updateLocalStorage();
+    }
+  });
+
+  const editt = todorow.querySelector(".edit");
+  editt.addEventListener("click", function () {
+    edit.style.display = "flex";
     const editing = document.querySelector(".editing");
-    let edited = editing.value.trim();
-    const blocktodo = current.querySelector("h2");
-    blocktodo.textContent = edited
-    const index = todos.indexOf(blocktodo.textContent);
-        if(index!== -1){
-            todos[index] = edited;
-        }
+    editing.value = todoText;
+    current = todorow;
+  });
+}
 
-        edit.style.display= "none";
-})
+function addingg() {
+  const todoText = inputodo.value.trim();
+  if (todoText === "") return;
 
-completed.addEventListener("click", function(){
-    const blocks = document.querySelectorAll(".block");
-    blocks.forEach((block)=>{
-    const checkbox = block.querySelector(".scales")
+  createTodoElement(todoText);
+  todos.push({ text: todoText, done: false });
+  updateLocalStorage();
 
-    if(!checkbox.checked){
-        block.style.display = "none";
-    }
-    else{
-        block.style.display = "flex";
+  inputodo.value = "";
+}
 
-    }
+loadTodos();
 
-    })
-})
+save.addEventListener("click", function () {
+  const editing = document.querySelector(".editing");
+  let edited = editing.value.trim();
+  const blocktodo = current.querySelector("h2");
+  const oldText = blocktodo.textContent;
+  blocktodo.textContent = edited;
 
-all.addEventListener("click", function(){
-    const blocks = document.querySelectorAll(".block");
-    blocks.forEach((block)=>{
-        block.style.display = "flex";
+  const index = todos.findIndex((todo) => todo.text === oldText);
+  if (index !== -1) {
+    todos[index].text = edited;
+    updateLocalStorage();
+  }
 
+  edit.style.display = "none";
+});
 
-    })
-})
-
-pending.addEventListener("click", function(){
-    const blocks = document.querySelectorAll(".block");
-    blocks.forEach((block)=>{
-    const checkbox = block.querySelector(".scales")
-
-    if(!checkbox.checked){
-        block.style.display = "flex";
-    }
-    else{
-        block.style.display = "none";
-
-    }
-
-    })
-})
-clear.addEventListener("click", function(){
-    modal.style.display = "flex";
-})
-cleard.addEventListener("click", function(){
-
-    modal.style.display = "none";
-    added.innerHTML = "";
-    todos = [];
-
-})
-cancel.addEventListener("click", function(){
-    modal.style.display = "none";
-
-})
-
-
-cedit.addEventListener("click", function(){
-    edit.style.display="none"
-})
-
-add.addEventListener("click", addingg)
-
-inputodo.addEventListener("keydown", function(event){
-    if(event.key === "Enter"){
-        addingg();
-    }
-})
-
-console.log(todos);
+completed.addEventListener("click", function () {
+  const blocks = document.querySelectorAll(".block");
+  blocks.forEach((block) => {
+    const checkbox = block.querySelector(".scales");
+    block.style.display = checkbox.checked ? "flex" : "none";
+  });
+});
+all.addEventListener("click", function () {
+  const blocks = document.querySelectorAll(".block");
+  blocks.forEach((block) => {
+    block.style.display = "flex";
+  });
+});
+pending.addEventListener("click", function () {
+  const blocks = document.querySelectorAll(".block");
+  blocks.forEach((block) => {
+    const checkbox = block.querySelector(".scales");
+    block.style.display = checkbox.checked ? "none" : "flex";
+  });
+});
+clear.addEventListener("click", function () {
+  modal.style.display = "flex";
+});
+cleard.addEventListener("click", function () {
+  modal.style.display = "none";
+  added.innerHTML = "";
+  todos.length = 0;
+  updateLocalStorage();
+});
+cancel.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+cedit.addEventListener("click", function () {
+  edit.style.display = "none";
+});
+add.addEventListener("click", addingg);
+inputodo.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    addingg();
+  }
+});
