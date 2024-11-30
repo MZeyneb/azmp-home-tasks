684a22e7ff5ac5dc04ed5aa178ec00c321008d7f
mzeyneb <zeynebmustafa05@gmail.com>
2024-11-30 18:28:47 +0400

    done

---
+++ 04.arrayMethods/script.js
@@ -0,0 +1,92 @@
+let arr = [
+    {
+      name: "test",
+      key: 1,
+    },
+    {
+      name: "task",
+      key: 2,
+    },
+    {
+      name: "tanqo",
+      key: 3,
+    },
+    {
+      name: "like",
+      key: 4,
+    },
+    {
+      name: "task",
+      key: 5,
+    },
+    {
+      name: "trust",
+      key: 6,
+    },
+    {
+      name: "test",
+      key: 7,
+    },
+    {
+      name: "last",
+      key: 8,
+    },
+    {
+      name: "tanqo",
+      key: 9,
+    },
+    {
+      name: "elephant",
+      key: 10,
+    },
+    {
+      name: "love",
+      key: 11,
+    },
+    {
+      name: "small",
+      key: 12,
+    },
+    {
+      name: "little",
+      key: 13,
+    },
+  ];
+
+
+// 1
+
+// let newArr = arr.filter(obj=> obj.name.startsWith("t"));
+// console.log(newArr);
+
+
+// 2
+
+// let newArr = arr.filter(obj=> obj.name.startsWith("t") && obj.name.endsWith("t"))
+// console.log(newArr.length);
+
+
+// 3
+// let sum = 0;
+// newArr.forEach(element => {
+//     sum+= element.key;
+
+    
+// });
+
+// console.log(sum);
+
+
+// 4
+
+// arr.forEach(element=>{
+//     if (element.name.startsWith("t")){
+//         element.name = "SuperDev"
+//     }
+// })
+
+// console.log(arr);
+
+
+// 5
+
