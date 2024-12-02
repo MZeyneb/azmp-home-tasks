026f7ea200fd35001d06a37c56a73da4c078ba06
MZeyneb <130896542+MZeyneb@users.noreply.github.com>
2024-12-02 12:01:55 +0400

    Add files via upload
    pushed

--- 21.11js(basket)/home.js
+++ 21.11js(basket)/home.js
@@ -2,9 +2,11 @@ import { carsData } from "./data.js";
 
 const cards = document.querySelector(".cards");
 let data = []
+let basket = JSON.parse(localStorage.getItem("basket")) || []; // Retrieve basket data or initialize as an empty array
+let add = document.querySelectorAll(".add")
 function rendering(arr){
     cards.innerHTML = "";
-    arr.forEach((car)=>{
+    arr.forEach((car, index)=>{
         const card = document.createElement("div");
         card.classList.add("card")
         card.innerHTML=`
@@ -31,7 +33,7 @@ function rendering(arr){
                         </div>
                         <div class="price">
                             <span>$${car.price}</span>
-                            <button class="add">Add to Basket</button>
+                            <button class="add" data-index="${index}">Add to Basket</button>
                         </div>
                         
                         </div>
@@ -47,4 +49,21 @@ function rendering(arr){
 
 }
 
+add.forEach(button => {
+    button.addEventListener("click", (event)=>{
+        const index = event.target.dataset.index;
+        const selected = arr[index];
+        addToBasket(selected)
+
+        
+    })
+    
+});
+
+
+function addToBasket(car) {
+    basket.push(car);
+    localStorage.setItem("basket", JSON.stringify(basket));
+    window.location.href = "basket.html"; 
+}
 rendering(carsData)
